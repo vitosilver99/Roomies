4ce4a128fbce9558dc36f3aab5e3c16f7373f1e2
98Alessandro98 <alessandro.maione.ba@gmail.com>
2020-12-17 23:54:04 +0100

    completamento parziale del fragment pagamento. Inizio dell'activity relativa ai dettagli del pagamento

--- app/src/main/java/com/example/roomies/MainActivity.java
+++ app/src/main/java/com/example/roomies/MainActivity.java
@@ -7,20 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // parametri inizializzazione PagamentiFragment
+    private static final String ARG_USER_ID = "param1";
+    private static final String ARG_CASA_ID = "param2";
+
+
+
     //FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    String UdCasa;
+    String userID;
+    String casaID;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
         Bundle b = new Bundle();
         b = getIntent().getExtras();
-        String parametri = b.getString("userID");
-        UdCasa = b.getString("casaID");
+        userID = b.getString("userID");
+        casaID = b.getString("casaID");
 
         fStore = FirebaseFirestore.getInstance();
 
@@ -39,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
 
-        Toast.makeText(getApplicationContext(), "userID:" + parametri, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "userID:" + userID, Toast.LENGTH_LONG).show();
 
 
     }
@@ -58,17 +64,25 @@ public class MainActivity extends AppCompatActivity {
                                 selectedFragment = new SpesaFragment();
                                 break;
                             case R.id.navigation_pagamenti:
+
+                                //devo passare al nuovo fragment casaID cioè l'ID della casa (lo passo in param1)
+                                Bundle args= new Bundle();
+                                args.putCharSequence(ARG_USER_ID, userID);
+                                args.putCharSequence(ARG_CASA_ID, casaID);
                                 selectedFragment = new PagamentiFragment();
+                                selectedFragment.setArguments(args);
+
                                 break;
                             case R.id.navigation_chat:
                                 selectedFragment = new ChatFragment();
                                 break;
                             case R.id.navigation_calendario:
-                                selectedFragment = new CalendarFragment(UdCasa);
+                                selectedFragment = new CalendarFragment(casaID);
                                 break;
                         }
                         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
                         return true;
+
                     }
     };
 
